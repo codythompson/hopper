fd68bd54c99a8637ca3d3a69bf7b5ff162222d0d
Cody Thompson <cody.thompson22@gmail.com>
2018-01-06 17:17:31 -0800

    created a body cache

---
+++ src/ChunkBodyCache.js
@@ -0,0 +1,128 @@
+let priv = null;
+
+/**
+ * ChunkBodyCache
+ * Stores a limited amount of chunks' p2 bodies in memory
+ */
+class ChunkBodyCache {
+  /**
+   * @constructor
+   * @param {*} args the arguments object
+   * @param {p2.World} args.world the p2.World instance that all of the entitys exist in
+   * @param {number} [args.size=256] the maximum number of bodies to store
+   */
+  constructor (args) {
+    if (typeof args !== 'object') {
+      throw '[hopper][ChunkBodyCache][constructor] an arguments object is required.';
+    }
+    if (!args.world) {
+      throw '[hopper][ChunkBodyCache][constructor] "world" is a required arg.';
+    }
+    args = _.defaults(args, {
+      size: 16*16
+    });
+
+    let cache = [];
+    for (let i = 0; i < args.size; i++) {
+      cahce.push({
+        body: null,
+        i: null,
+        j: null
+      });
+    }
+
+    priv = {
+      cache: cache,
+      cachePtr: 0,
+      cacheMap: {}
+    };
+  }
+
+  /**
+   * @method deleteCacheMapEntry
+   * Removes entries from the internal cache map
+   * 
+   * @param {number} i chunk column index
+   * @param {number} j chunk row index
+   */
+  deleteCacheMapEntry (i, j) {
+    if (this.hasBody(i, j)) {
+      let rowMap = priv.cacheMap[i];
+      delete rowMap[j];
+      if (Object.keys(rowMap) === 0) {
+        delete priv.cacheMap[i];
+      }
+    }
+  }
+
+
+  /**
+   * @method addCacheMapEntry
+   * Removes entries from the internal cache map
+   * 
+   * @param {number} i chunk column index
+   * @param {number} j chunk row index
+   * @param {number} ix the internal cache pointer
+   */
+  addCacheMapEntry (i, j, ptr) {
+    if (!(i in priv.cacheMap)) {
+      priv.cacheMap[i] = {};
+    }
+    if (!(j in priv.cacheMap[i])) {
+      priv.cacheMap[i][j] = ptr;
+    }
+  }
+
+  hasBody (i, j) {
+    return i in priv.cacheMap && j in priv.cacheMap[i];
+  }
+
+  getBody (i, j) {
+    if (this.hasBody(i,j)) {
+      let ix = priv.cacheMap[i, j];
+      return priv.cache[ix];
+    } else {
+      return null;
+    }
+  }
+
+  addBody (body, i, j) {
+    // if we already have an entry for i, j
+    // replace the body if it's different
+    if (this.hasBody(i,j)) {
+      let ix = priv.cacheMap[i,j];
+      let oldBody = priv.cache[ix].body;
+      if (oldBody !== body) {
+        priv.world.removeBody(oldBody);
+        priv.world.addBody(body);
+        priv.cache[ix].body = body;
+      }
+    } else {
+      let cache = priv.cache[priv.cachePtr];
+
+      // if we are overwriting an existing entry
+      // lets clear it out of the cachemap
+      // and remove it from the world
+      this.deleteCacheMapEntry(cache.i, cache.j);
+      priv.world.removeBody(cache.body);
+
+      // reset the cache entry
+      priv.world.addBody(body);
+      cache.i = i;
+      cache.j = j;
+      cache.body = body;
+
+      // increment the cache pointer and rollover if necessary
+      priv.cachePtr++;
+      if (priv.cachePtr === priv.cache.length) {
+        priv.cachePtr = 0;
+      }
+    }
+  }
+
+  get size () {
+    return priv.cache.length;
+  }
+}
+
+module.exports = ChunkBodyCache;
--- src/ChunkShapeBuilder.js
+++ src/ChunkShapeBuilder.js
@@ -1,6 +1,28 @@
+const ChunkBodyCache = require('./ChunkBodyCache');
+
+let priv = null;
+
 class ChunkBodyBuilder {
+  /**
+   * @constructor
+   * @param {*} args the arguments object
+   * @param {object} args.buildMap an object that maps a shapeId to an instance of BlockShapeBuilder
+   * @param {number} [args.bodyCacheSize] the size of the cache that will store the bodys
+   */
   constructor (args) {
+    if (typeof args !== 'object') {
+      throw '[hopper][ChunkBodyBuilder][constructor] an arguments object is required.';
+    }
+    if (!args.builderMap) {
+      throw '[hopper][ChunkBodyBuilder][constructor] "builderMap" is a required arg.';
+    }
     this.builderMap = args.builderMap;
+
+    priv = {
+      bodyCache: new ChunkBodyCache({
+        size: args.bodyCacheSize
+      })
+    };
   }
 
   buildShape (block) {
@@ -12,6 +34,13 @@ class ChunkBodyBuilder {
   }
 
   buildBody (camera, chunkI, chunkJ) {
+    // if the body exists in the cache, return it
+    if (priv.bodyCache.hasBody(chunkI, chunkJ)) {
+      return priv.bodyCache.getBody(chunkI, chunkJ);
+    }
+
+    // if the body doesn't exist in the cache,
+    // build it and add it to the cache
     let pos = camera.chunkToWorld (chunkI, chunkJ);
     let body = new p2.Body({
       mass: 0,
@@ -27,5 +56,7 @@ class ChunkBodyBuilder {
         }
       }
     }
+    priv.bodyCache.addBody(body, chunkI, chunkJ);
+    return body;
   }
 }
